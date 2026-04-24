fb34daec30c07e7ace1dc2b64ddb8ab8d9569936
angelia3443 <erikaangelia2005@gmail.com>
2026-04-24 12:37:07 +0700

    test2

--- models/item.py
+++ models/item.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nama = db.Column(db.String(100), nullable=False)
+    nama = db.Column(db.String(100), nullable=False) # MySQL butuh angka panjang (100)
     jumlah = db.Column(db.Integer, default=0)
 
     def to_dict(self):
